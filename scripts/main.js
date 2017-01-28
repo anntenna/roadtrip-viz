4184641624574aeaba005293b428bbfd06d3f58f
anusha <anushab@outlook.com>
2017-01-27 16:08:15 -0800

    Added more info, modified trips to animate as round trips

--- scripts/main.js
+++ scripts/main.js
@@ -7,15 +7,17 @@ var routeFilesExt = ".geojson";
 var timeScale = d3.scaleTime();
 var triplogGlobal;
 
-var timeSvg = d3.select("div").append("svg")
+var timeSvg = d3.select("div#timeline").append("svg")
 	.attr("width", w)
 	.attr("height", timeh)
 	.attr("id", "timeline");
 
-var mapSvg = d3.select("div").append("svg")
+var mapSvg = d3.select("div#map").append("svg")
 	.attr("width", w)
 	.attr("height", h)
 	.attr("id", "map");
+
+var marker;
 
 var projection = d3.geoAlbers();
 
@@ -46,7 +48,8 @@ function ready(error, usStates, triplog) {
 }
 
 function loadMap(usStates) {
-	var usagroup = mapSvg.append("g").attr("class", "usa");
+	var usagroup = mapSvg.append("g").attr("class", "usa")
+		.attr("z-index",-1);
 
 	usagroup.selectAll("path")
 		.data(usStates.features)
@@ -62,8 +65,24 @@ function parseLog(error, results) {
 	//draw time axis
 	var firstStartdate = triplogGlobal[0].start_date;
 	drawTimeline(firstStartdate);
+
+	var routegroup = mapSvg.append("g").attr("class", "routeGroup")
+
+	var markerGroup = mapSvg.append("g").attr("class", "marker");
 	
+	marker = markerGroup.append("circle")
+		.classed("routeMarker", true)
+		.attr("r", 5)
+		.attr("transform", "translate(-50000,-50000)")
+	
+	var thisRouteDetailsDiv = d3.selectAll("span");
+	thisRouteDetailsDiv.text(0);
+
+	
+
 	animate(results);
+
+	
 
 	logDataCF = crossfilter(triplogGlobal);
 	startDateDimension = logDataCF.dimension(function(d) { return d.start_date; });
@@ -132,6 +151,7 @@ function animate(routes) {
 	var timeCircle = timeSvg.select("#timeCircle");
 
 	var animateq = d3.queue();
+	var totalMiles = 0;
 	var i = 0;
 
 	triplogGlobal.forEach(function(d) {
@@ -150,10 +170,78 @@ function animate(routes) {
 			.delay(i * 2000)
 			.on("end", renderRoute());
 
+		var tweenCalc = function(x, selector) {
+			var elem = d3.select(selector);
+			var i = d3.interpolate(elem.node().textContent, d.miles);
+      			return function(t) {
+					elem.text(i(t));
+      			};
+		};
 		
+		
+		//Edit div
+		var thisRouteDetailsDiv = d3.select("span#tripMiles");
+		thisRouteDetailsDiv.transition()
+			.ease(d3.easeQuadOut)
+			.duration(1000)
+			.delay(1000*(i+1) + 1000*i)
+			.tween("text", function(b) {
+				var i = d3.interpolate(d3.select("span#tripMiles").node().textContent, d.miles);
+				return function(t) {
+					d3.select("span#tripMiles").text(parseInt(i(t),10));
+				};
+			});
+
+		var overallMilesDiv = d3.select("span#overallMiles");
+		overallMilesDiv
+			.transition()
+			.ease(d3.easeQuadOut)
+			.duration(1000)
+			.delay(1000*(i+1) + 1000*i)
+			.tween("text", function(b) {
+				var i = d3.interpolate(d3.select("span#overallMiles").node().textContent, parseInt(d3.select("span#overallMiles").node().textContent, 10) + d.miles);
+				return function(t) {
+					d3.select("span#overallMiles").text(parseInt(i(t),10));
+				};
+			});
+
+		var tripNamePara = d3.select("p#tripNamePara");
+		tripNamePara
+			.transition()
+			.text(d.name)
+			.ease(d3.easeQuadOut)
+			.duration(1000)
+			.delay(1000*(i+1) + 1000*i);
+
+		var tripDaysPara = d3.select("span#tripDays");
+		tripDaysPara
+			.transition()
+			.ease(d3.easeQuadOut)
+			.duration(1000)
+			.delay(1000*(i+1) + 1000*i)
+			.tween("text", function(b) {
+				var i = d3.interpolate(d3.select("span#tripDays").node().textContent, d.duration_days);
+				return function(t) {
+					d3.select("span#tripDays").text(parseInt(i(t),10));
+				};
+			});
+
+		var overallDaysDiv = d3.select("span#overallDays");
+		overallDaysDiv
+			.transition()
+			.ease(d3.easeQuadOut)
+			.duration(1000)
+			.delay(1000*(i+1) + 1000*i)
+			.tween("text", function(b) {
+				var i = d3.interpolate(d3.select("span#overallDays").node().textContent, parseInt(d3.select("span#overallDays").node().textContent, 10) + d.duration_days);
+				return function(t) {
+					d3.select("span#overallDays").text(parseInt(i(t),10));
+				};
+			});
+		
+
 		i++;
-		
-		
+
 	});
 
 }
@@ -170,7 +258,8 @@ function drawRoute(route, j) {
 	route.features[0].geometry.coordinates = coords;
 	route.features.splice(1, route.features.length-1);
 
-	var routegroup = mapSvg.append("g").attr("class", "routeGroup")
+	var routegroup = mapSvg.select("g.routeGroup").append("g").attr("class","route");
+
 
 	var linePath = routegroup.selectAll("path")
 		.data(route.features)
@@ -179,15 +268,50 @@ function drawRoute(route, j) {
 		.attr("d", path)
 		.classed("route", true);
 
+	//Get path start point for placing marker
+	function pathStartPoint(path) {
+		var d = path.attr("d"),
+		dsplitted = d.split("L");
+		return dsplitted[1].split(",");
+	}
+
+	var startPoint = pathStartPoint(linePath);
+
+	marker.attr("transform", "translate(" + startPoint +")");
+
+	
+	function translateAlong(path) {
+    	var l = path.getTotalLength();
+    	return function(i) {
+      		return function(t) {
+        		var p = path.getPointAtLength(t * l);
+        		return "translate(" + p.x + "," + p.y + ")";//Move marker
+      		}
+    	}
+  	}
+
 	var totalLength = linePath.node().getTotalLength();
 
 	linePath
       .attr("stroke-dasharray", totalLength + " " + totalLength)
       .attr("stroke-dashoffset", totalLength)
+	  .classed("active", true)
       .transition()
         .duration(1000)
         .ease(d3.easeLinear)
         .attr("stroke-dashoffset", 0)
-		.delay(1000*(j+1) + 1000*j);
+		.delay(1000*(j+1) + 1000*j)
+		.transition()
+		.style("stroke-width", "1px");
+	
+	marker.transition()
+		.duration(1000)
+		.attrTween("transform", translateAlong(linePath.node()))
+		.ease(d3.easeLinear)
+		.delay(1000*(j+1) + 1000*j)
+
+	
 }
+
+
 
